eabd1773730ad74c440c0e9ac4877919a8690021
nutellayan <157748332+nutellayan@users.noreply.github.com>
2024-06-07 20:48:59 +0100

    Unit 2 - Practise 1

---
+++ src/main/java/com/napier/sem2/One.java
@@ -0,0 +1,32 @@
+package com.napier.sem2;
+
+import javax.swing.JOptionPane;
+
+public class One {
+    public static void main(String[] args) {
+        String firstNumberString = JOptionPane.showInputDialog("Enter the first number");
+        String secondNumberString = JOptionPane.showInputDialog("Enter the second number");
+
+        // Convert input from String to int
+        int firstNumber = Integer.parseInt(firstNumberString);
+        int secondNumber = Integer.parseInt(secondNumberString);
+
+        // Calculation
+        int sum = firstNumber + secondNumber;
+        int multiply = firstNumber * secondNumber;
+
+        // Print message
+        String resultMessage = "The sum of the numbers is: " + sum + "\n" +
+                "The product of the numbers is: " + multiply + "\n";
+
+        if (firstNumber > secondNumber) {
+            resultMessage += "The first number is larger.";
+        } else if (secondNumber > firstNumber) {
+            resultMessage += "The second number is larger.";
+        } else {
+            resultMessage += "Both numbers are equal.";
+        }
+
+        JOptionPane.showMessageDialog(null, resultMessage);
+    }
+}
