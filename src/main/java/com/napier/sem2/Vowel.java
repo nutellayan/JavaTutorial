ec5f1cf34b1fa4d21e8b03eb30a10f4d4e2fdaba
nutellayan <157748332+nutellayan@users.noreply.github.com>
2024-06-08 21:08:37 +0100

    Unit 2 - Vowel

---
+++ src/main/java/com/napier/sem2/Vowel.java
@@ -0,0 +1,23 @@
+package com.napier.sem2;
+
+import javax.swing.JOptionPane;
+
+public class Vowel {
+    public static void main(String[] args) {
+        String output;
+        String charAsString;
+        char theChar;
+
+        charAsString = JOptionPane.showInputDialog("Please enter a character");
+        theChar = charAsString.charAt(0);
+
+        if (theChar == 'a' || theChar == 'e' || theChar == 'i' || theChar == 'o' || theChar == 'u' ||
+                theChar == 'A' || theChar == 'E' || theChar == 'I' || theChar == 'O' || theChar == 'U') {
+            output = theChar + " is a vowel.";
+        } else {
+            output = theChar + " is a consonant.";
+        }
+
+        JOptionPane.showMessageDialog(null, output, "Character Check", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
